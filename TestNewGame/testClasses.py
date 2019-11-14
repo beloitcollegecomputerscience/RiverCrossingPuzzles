55eb68d1c2113090bdf11cf306d39f80aba816f7
Pathik Rupwate <57154523+pathik10@users.noreply.github.com>
2019-11-14 14:22:16 -0600

    TestSetNewGame by Pathik and Jamie

---
+++ TestNewGame/testClasses.py
@@ -0,0 +1,11 @@
+'''
+Created on Oct 29, 2019
+
+@author: Pathik
+'''
+class testClasses():
+    def load(self, gameName):
+        print("load Class ") 
+    def setNewGame(self, gameName):
+        print("setNewGame Class")
+    
