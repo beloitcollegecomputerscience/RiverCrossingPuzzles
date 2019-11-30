c74577ec82d1efcb5789619090ffc4af0eb62ec8
Leonora Tindall <nora@nora.codes>
2019-11-30 12:21:58 -0600

    Remove unnecessary duplication of Boat class

--- src/Boat.py
+++ src/Boat.py
@@ -8,21 +8,25 @@ An unimplemented Boat class with method stubs
 class Boat:
     boatSize = 0
     occupiedSeats = 0
-    
+
     def getSpaces(self):
         pass
-    
+
     def addRider(self):
         pass
-        
+
     def removeRider(self):
         pass
 
     def changeSize(self, x):
         pass
-    
+
     def isFull(self):
         pass
 
     def getShore(self):
         pass
+
+    def moveToShore(self):
+        pass
+
--- src/boat_shore_tests.py
+++ src/boat_shore_tests.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import unittest
-from game_object_mocks import GameState, Boat
+import GameState, Boat
 
 class TestBoatMethods(unittest.TestCase):
 
@@ -10,12 +10,12 @@ class TestBoatMethods(unittest.TestCase):
 
     def test_boat_explicitly_left_is_left(self):
         state = GameState()
-        state.boat.move_to_shore("left")
+        state.boat.moveToShore("left")
         self.assertEqual(state.boat.getShore(), "left")
 
     def test_boat_explicitly_right_is_right(self):
         state = GameState()
-        state.boat.move_to_shore("right")
+        state.boat.moveToShore("right")
         self.assertEqual(state.boat.getShore(), "right")
         
 if __name__ == '__main__':
