27e3142520fa945a548cd675e88ae2e61a916e94
Christian <farescs@beloit.edu>
2019-12-05 14:41:27 -0600

    Updated makeMove tests for Nora's PR.

--- riverCrossingPuzzles/filesForMakeMoveTest/LocationDummy.py
+++ riverCrossingPuzzles/filesForMakeMoveTest/LocationDummy.py
@@ -1,8 +1,8 @@
 def checkLocation(self, char_id):
     """return True to say that the character is in the right place."""
-    return True
+    pass
 
 
 def updateLocation(self, char_id, new_location):
     """update location to say that the character is in the right place."""
-    return True
+    pass
--- tests/ControlTestForMakeMove.py
+++ tests/ControlTestForMakeMove.py
@@ -1,5 +1,6 @@
 import unittest
-from RiverCrossingPuzzles.TestsForMakeMove import LocationDummy, ValidationDummy
+
+from riverCrossingPuzzles.filesForMakeMoveTest import LocationDummy, ValidationDummy
 
 """Checks with Validation class if the move is valid. If yes, update the Location class and call the Animation."""
 
@@ -10,12 +11,8 @@ class ControlTest(unittest.TestCase):
         """Checks that this is the character we want to work with"""
         self.assertEqual(char_id, 2)
         """Checks that the character is in the right place"""
-        self.assertTrue(LocationDummy.checkLocation(self, char_id), True)
+        self.assertTrue(LocationDummy.checkLocation(self, char_id), None)
         """Checks that the move is legal"""
-        self.assertTrue(ValidationDummy.isLegal(self, move, char_id), True)
+        self.assertTrue(ValidationDummy.isLegal(self, move, char_id), None)
         """Updates location"""
-        self.assertTrue(LocationDummy.updateLocation(self, char_id, 'new location'), True)
-
-
-if __name__ == '__main__':
-    unittest.main()
+        self.assertTrue(LocationDummy.updateLocation(self, char_id, 'new location'), None)
