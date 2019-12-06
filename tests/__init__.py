e6e17f0fa3dda176dd2920e5f65098cbb87115ac
Corina Sterbet <sterbetc@beloit.edu>
2019-12-05 21:37:08 -0600

    Update __init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,4 @@
 from .BoatOverfilledTest import BoatOverfilledTest
 from .BoatShoreTests import BoatShoreTests
-<<<<<<< HEAD
-from .ControlTestForMakeMove import ControlTest
-=======
 from .ControlTestForMakeMove import ControlTestForMakeMove
 from .BoatSpacesTests import BoatSpacesTests
-
->>>>>>> 59b0570c4637e38a10c2ce41baf48416fb679e3f
