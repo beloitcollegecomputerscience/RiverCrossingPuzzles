4de3b68be9d41a5ad3def04e13fbda68f7ffe5fd
Corina <sterbetc@beloit.edu>
2020-02-06 14:38:45 -0600

    Add some enhancements to the original pyglet animation code: 1) include intermediate game states for the characters 2) at the end (win state), the characters leave the screen 3) add a "click" test: if the goat is clicked, the console outputs "Goat was clicked"

--- Animation/animation.py
+++ Animation/animation.py
@@ -29,7 +29,10 @@ large_bottom_margin = 150
 small_bottom_margin = 100
 each_height = 130
 each_width = 100
-clicked = False
+global_state = 'initialized'
+
+last_time = 0
+
 window = pyglet.window.Window(img_background.width, img_background.height)
 @window.event
 def on_draw():
@@ -42,38 +45,70 @@ def on_draw():
 	sprite_4.draw()
 @window.event
 def on_mouse_press(x, y, button, modifiers):
-	global clicked 
-	clicked = True
+	global global_state 
+	if global_state == 'initialized':
+		global_state = 'clicked'
+
+	if goat_clicked(x, y):
+		print("Goat was clicked")
 	pass
 
 def update(duration):
-	global clicked
+	global global_state
+	global last_time
 	velocity = 300
 
-	destination_3_x = hor_gap
-	destination_4_x = hor_gap
-	destination_2_x = hor_gap + each_width
-	destination_1_x = hor_gap + each_width
+	if global_state != 'timeout_before_win':
+		destination_3_x = hor_gap
+		destination_4_x = hor_gap
+		destination_2_x = hor_gap + each_width
+		destination_1_x = hor_gap + each_width
 
-	destination_1_y = large_bottom_margin + each_height + ver_gap
-	destination_4_y = small_bottom_margin + each_height + ver_gap
-	destination_2_y= large_bottom_margin + each_height * 2 + ver_gap * 2
-	destination_3_y = small_bottom_margin + each_height * 2 + ver_gap * 2
-	destination_boat_x = 500
+		destination_1_y = large_bottom_margin + each_height + ver_gap
+		destination_4_y = small_bottom_margin + each_height + ver_gap
+		destination_2_y= large_bottom_margin + each_height * 2 + ver_gap * 2
+		destination_3_y = small_bottom_margin + each_height * 2 + ver_gap * 2
+		destination_boat_x = 500
 
-	if clicked == True:
+	if global_state == 'clicked':
 		destination_1_x += river
 		destination_2_x += river
 		destination_3_x += river + hor_gap * 2 + each_width * 2
 		destination_4_x += river + hor_gap * 2 + each_width * 2
 		animate(boat, destination_boat_x, boat.y, duration, velocity)
 
-	animate(sprite_1, destination_1_x, destination_1_y, duration, velocity)
-	animate(sprite_2, destination_2_x, destination_2_y, duration, velocity)
-	animate(sprite_3, destination_3_x, destination_3_y, duration, velocity)
-	animate(sprite_4, destination_4_x, destination_4_y, duration, velocity)
+	if global_state == 'timeout_before_win':
+		if timeout_ended(1):
+			global_state = 'game_win'
+		return
+
+	if global_state == 'game_win':
+		out_from_screen = 300
+		destination_1_x += river + out_from_screen
+		destination_2_x += river + out_from_screen
+		destination_3_x += river + hor_gap * 2 + each_width * 2 + out_from_screen
+		destination_4_x += river + hor_gap * 2 + each_width * 2 + out_from_screen
+
+	is_animating_1 = animate(sprite_1, destination_1_x, destination_1_y, duration, velocity)
+	is_animating_2 = animate(sprite_2, destination_2_x, destination_2_y, duration, velocity)
+	is_animating_3 = animate(sprite_3, destination_3_x, destination_3_y, duration, velocity)
+	is_animating_4 = animate(sprite_4, destination_4_x, destination_4_y, duration, velocity)
+	# print(is_animating_1)
+	if global_state == 'clicked' and check_if_all_stopped(is_animating_1, is_animating_2, is_animating_3, is_animating_4):
+		global_state = 'timeout_before_win'
+		last_time = time.time()
+
+	if global_state == 'game_win' and check_if_all_stopped(is_animating_1, is_animating_2, is_animating_3, is_animating_4):
+		global_state = 'initialized'
+		reset_sprites_positions()
+		
+
+	# print(global_state)
 
 def animate(sprt, destination_x, destination_y, duration, velocity):
+	if check_if_sprite_at_destination(sprt, destination_x, destination_y):
+		return False
+
 	if sprt.x < destination_x - 1:
 		sprt.x += velocity * duration
 	if sprt.x > destination_x + 1:
@@ -83,7 +118,9 @@ def animate(sprt, destination_x, destination_y, duration, velocity):
 	if sprt.y > destination_y + 1:
 		sprt.y -= velocity * duration
 
-def check(sprt, destination_x, destination_y):
+	return True	
+
+def check_if_sprite_at_destination(sprt, destination_x, destination_y):
 	if sprt.x < destination_x - 3:
 		return False
 	if sprt.x > destination_x + 3:
@@ -93,6 +130,37 @@ def check(sprt, destination_x, destination_y):
 	if sprt.y > destination_y + 3:
 		return False
 	return True
+
+def check_if_all_stopped(is_animating_1, is_animating_2, is_animating_3, is_animating_4):
+	still_animating = is_animating_1 or is_animating_2 or is_animating_3 or is_animating_4
+	return not still_animating
+
+def timeout_ended(timeout_seconds):
+	return (time.time() - last_time) > timeout_seconds
+
+def reset_sprites_positions():
+	off_screen_y = 450
+	sprite_1.x = 0
+	sprite_1.y = off_screen_y
+	sprite_2.x = 0
+	sprite_2.y = off_screen_y
+	sprite_3.x = 0
+	sprite_3.y = off_screen_y
+	sprite_4.x = 0
+	sprite_4.y = off_screen_y
+	boat.x = 230
+	boat.y = 150
+
+def goat_clicked(x, y):
+	goat_collider_radius = 40
+	goat_center_x = sprite_1.x + goat_collider_radius
+	goat_center_y = sprite_1.y + goat_collider_radius
+	
+	if (abs(x - goat_center_x) < goat_collider_radius) and (abs(y - goat_center_y) < goat_collider_radius):
+		return True
+	return False
+
+
 
 if __name__ =='__main__':
 	pyglet.clock.schedule_interval(update, 1/200.0)
