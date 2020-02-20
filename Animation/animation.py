5a0019e3b83088e2946ff4c98b10f01696ca3056
Corina <sterbetc@beloit.edu>
2020-02-20 09:10:46 -0600

    Accomplished: 1) click on characters and make them get into/out of the boat (destinations: boat, shore); 2) when clicking on boat - 2 conditions are checked: a. if there are <= 2 characters in the boat AND b. the driver_is_present. If the conditions are passed, set the destination to the opposite shore for the boat as well as for all the characters on the boat; 3) reorganize the update method; 4) re-upload the boat image since its invisible margins have been cropped;

--- Animation/animation.py
+++ Animation/animation.py
@@ -19,7 +19,7 @@ class Animation(pyglet.window.Window):
 		pyglet.window.Window.__init__(self, width = self.images["background"].width, height = self.images["background"].height)
 		#These coordinates have been approved and can be used for any set of characters
 		self.background = pyglet.sprite.Sprite(img=self.images["background"], x=0, y = 0)
-		self.boat = pyglet.sprite.Sprite(img=self.images["boat"], x=230, y = 150)
+		self.boat = pyglet.sprite.Sprite(img=self.images["boat"], x=270, y = 218)
 		self.sprite_goat = pyglet.sprite.Sprite(img=self.images["goat"], x=0, y = 450)
 		self.sprite_wolf = pyglet.sprite.Sprite(img=self.images["wolf"], x=0, y = 450)
 		self.sprite_farmer = pyglet.sprite.Sprite(img=self.images["farmer"], x=0, y = 450)
@@ -34,6 +34,7 @@ class Animation(pyglet.window.Window):
 
 		#These values are based on the current background's size. Be careful when use different background.
 		self.river = 660
+		self.river_boat_travel_distance = 270
 		self.hor_gap = 10
 		self.ver_gap = 0
 		self.large_bottom_margin = 150
@@ -41,9 +42,11 @@ class Animation(pyglet.window.Window):
 		self.each_height = 130
 		self.each_width = 100
 
-		self.global_state = 'initialized'
+		self.global_state = 'left_shore'
 		self.last_time = 0
+		self.characters_on_board = []
 
+		self.set_destinations_initial()
 		pyglet.clock.schedule_interval(self.update, 1/200.0)
 
 	def on_draw(self):
@@ -51,62 +54,58 @@ class Animation(pyglet.window.Window):
 		self.background.draw()
 		self.boat.draw()
 		self.sprite_goat.draw()
-		self.sprite_wolf.draw()
 		self.sprite_farmer.draw()
 		self.sprite_cabbage.draw()
+		self.sprite_wolf.draw()
 
 	def on_mouse_press(self,x, y, button, modifiers):
-		if self.global_state == 'initialized':
-			self.global_state = 'clicked'
+		if self.global_state == 'left_shore':
+			if self.goat_clicked(x, y):
+				self.goat_clicked_actions(x, y)
+			elif self.farmer_clicked(x, y):
+				self.farmer_clicked_actions(x, y)
+			elif self.wolf_clicked(x, y):
+				self.wolf_clicked_actions(x, y)
+			elif self.cabbage_clicked(x, y):
+				self.cabbage_clicked_actions(x, y)
+			elif self.boat_clicked(x, y):
+				self.boat_try_ride(1, x, y)
 
-		if self.goat_clicked(x, y):
-			print("Goat was clicked")
+		elif self.global_state == 'right_shore':
+			if self.boat_clicked(x, y):
+				self.boat_try_ride(-1, x, y)
+
 		pass
 
 	def update(self, duration):
+		# print(global_state)
 		velocity = 300
 
-		if self.global_state != 'timeout_before_win':
-			self.destination_goat = [self.hor_gap + self.each_width, self.large_bottom_margin + self.each_height + self.ver_gap]
-			self.destination_wolf = [self.hor_gap + self.each_width, self.large_bottom_margin + self.each_height * 2 + self.ver_gap * 2]
-			self.destination_farmer = [self.hor_gap, self.small_bottom_margin + self.each_height * 2 + self.ver_gap * 2]
-			self.destination_cabbage = [self.hor_gap, self.small_bottom_margin + self.each_height + self.ver_gap]
-			self.destination_boat = [500, 150]
+		# # these states aren't used for now
+		# if self.global_state == 'timeout_before_win':
+		# 	if self.timeout_ended(1):
+		# 		self.global_state = 'game_win'
+		# 	return
 
-		# Positions of characters and boat on the right side of the river
-		if self.global_state == 'clicked':
-			self.destination_goat[0] += self.river
-			self.destination_wolf[0] += self.river
-			self.destination_farmer[0] += self.river + self.hor_gap * 2 + self.each_width * 2
-			self.destination_cabbage[0] += self.river + self.hor_gap * 2 + self.each_width * 2
-			self.animate(self.boat, self.destination_boat, duration, velocity)
-
-		if self.global_state == 'timeout_before_win':
-			if self.timeout_ended(1):
-				self.global_state = 'game_win'
-			return
-
-		if self.global_state == 'game_win':
-			out_from_screen = 300
-			self.destination_goat[0] += self.river + out_from_screen
-			self.destination_wolf[0] += self.river + out_from_screen
-			self.destination_farmer[0] += self.river + self.hor_gap * 2 + self.each_width * 2 + out_from_screen
-			self.destination_cabbage[0] += self.river + self.hor_gap * 2 + self.each_width * 2 + out_from_screen
+		# if self.global_state == 'game_win':
+		# 	out_from_screen = 300
+		# 	self.set_destinations()
+		# 	self.destination_goat[0] += self.river + out_from_screen
+		# 	self.destination_wolf[0] += self.river + out_from_screen
+		# 	self.destination_farmer[0] += self.river + self.hor_gap * 2 + self.each_width * 2 + out_from_screen
+		# 	self.destination_cabbage[0] += self.river + self.hor_gap * 2 + self.each_width * 2 + out_from_screen
 
 		self.is_animating_1 = self.animate(self.sprite_goat, self.destination_goat, duration, velocity)
 		self.is_animating_2 = self.animate(self.sprite_wolf, self.destination_wolf, duration, velocity)
 		self.is_animating_3 = self.animate(self.sprite_farmer, self.destination_farmer, duration, velocity)
 		self.is_animating_4 = self.animate(self.sprite_cabbage, self.destination_cabbage, duration, velocity)
-		# print(is_animating_1)
-		if self.global_state == 'clicked' and self.check_if_all_stopped(self.is_animating_1, self.is_animating_2, self.is_animating_3, self.is_animating_4):
-			self.global_state = 'timeout_before_win'
-			self.last_time = time.time()
+		self.is_animating_boat = self.animate(self.boat, self.destination_boat, duration, velocity)
 
-		if self.global_state == 'game_win' and self.check_if_all_stopped(self.is_animating_1, self.is_animating_2, self.is_animating_3, self.is_animating_4):
-			self.global_state = 'initialized'
-			self.reset_sprites_positions()
+		# # not used for now
+		# if self.global_state == 'game_win' and self.check_if_all_stopped(self.is_animating_1, self.is_animating_2, self.is_animating_3, self.is_animating_4):
+		# 	self.global_state = 'left_shore'
+		# 	self.reset_sprites_positions()
 
-		# print(global_state)
 
 	def animate(self, sprt, destination, duration, velocity):
 		if self.check_if_sprite_at_destination(sprt, destination):
@@ -124,13 +123,13 @@ class Animation(pyglet.window.Window):
 		return True	
 
 	def check_if_sprite_at_destination(self, sprt, destination):
-		if sprt.x < destination[0] - 3:
+		if sprt.x < destination[0] - 5:
 			return False
-		if sprt.x > destination[0] + 3:
+		if sprt.x > destination[0] + 5:
 			return False
-		if sprt.y < destination[1] - 3:
+		if sprt.y < destination[1] - 5:
 			return False
-		if sprt.y > destination[1] + 3:
+		if sprt.y > destination[1] + 5:
 			return False
 		return True
 
@@ -138,7 +137,7 @@ class Animation(pyglet.window.Window):
 		self.still_animating = is_animating_1 or is_animating_2 or is_animating_3 or is_animating_4
 		return not self.still_animating
 
-	def timeout_ended(self,timeout_seconds):
+	def timeout_ended(self, timeout_seconds):
 		return (time.time() - self.last_time) > timeout_seconds
 
 	def reset_sprites_positions(self):
@@ -147,19 +146,150 @@ class Animation(pyglet.window.Window):
 		self.move_sprite_to(self.sprite_wolf, 0, off_screen_y)
 		self.move_sprite_to(self.sprite_farmer, 0, off_screen_y)
 		self.move_sprite_to(self.sprite_cabbage, 0, off_screen_y)
-		self.move_sprite_to(self.boat, 230, 150)
+		self.move_sprite_to(self.boat, 270, 218)
 
-	def move_sprite_to(self,sprite, x, y):
+	def set_destinations_initial(self):
+		self.destination_goat = [self.hor_gap + self.each_width, self.large_bottom_margin + self.each_height + self.ver_gap]
+		self.destination_wolf = [self.hor_gap + self.each_width, self.large_bottom_margin + self.each_height * 2 + self.ver_gap * 2]
+		self.destination_farmer = [self.hor_gap, self.small_bottom_margin + self.each_height * 2 + self.ver_gap * 2]
+		self.destination_cabbage = [self.hor_gap, self.small_bottom_margin + self.each_height + self.ver_gap]
+		self.destination_boat = [270, 218]
+
+	def set_destinations_to_other_shore(self, direction):
+		self.destination_boat[0] += direction * self.river_boat_travel_distance
+		if 'goat' in self.characters_on_board:
+			self.destination_goat[0] += direction * self.river_boat_travel_distance
+		if 'wolf' in self.characters_on_board:
+			self.destination_wolf[0] += direction * self.river_boat_travel_distance
+		if 'farmer' in self.characters_on_board:
+			self.destination_farmer[0] += direction * self.river_boat_travel_distance
+		if 'cabbage' in self.characters_on_board:
+			self.destination_cabbage[0] += direction * self.river_boat_travel_distance
+
+	def move_sprite_to(self, sprite, x, y):
 		sprite.x = x
 		sprite.y = y
 		return sprite
 
+	def set_goat_destination_to_boat(self, offset_x, offset_y):
+		self.destination_goat = [self.destination_boat[0] + offset_x, self.destination_boat[1] + offset_y]
+
+	def set_wolf_destination_to_boat(self, offset_x, offset_y):
+		self.destination_wolf = [self.destination_boat[0] + offset_x, self.destination_boat[1] + offset_y]
+
+	def set_farmer_destination_to_boat(self, offset_x, offset_y):
+		self.destination_farmer = [self.destination_boat[0] + offset_x, self.destination_boat[1] + offset_y]
+
+	def set_cabbage_destination_to_boat(self, offset_x, offset_y):
+		self.destination_cabbage = [self.destination_boat[0] + offset_x, self.destination_boat[1] + offset_y]
+
+	def set_goat_destination_to_shore(self):
+		self.destination_goat = [self.hor_gap + self.each_width, self.large_bottom_margin + self.each_height + self.ver_gap]
+
+	def set_wolf_destination_to_shore(self):
+		self.destination_wolf = [self.hor_gap + self.each_width, self.large_bottom_margin + self.each_height * 2 + self.ver_gap * 2]
+
+	def set_farmer_destination_to_shore(self):
+		self.destination_farmer = [self.hor_gap, self.small_bottom_margin + self.each_height * 2 + self.ver_gap * 2]
+
+	def set_cabbage_destination_to_shore(self):
+		self.destination_cabbage = [self.hor_gap, self.small_bottom_margin + self.each_height + self.ver_gap]
+
 	def goat_clicked(self,x, y):
 		goat_collider_radius = 40
-		goat_center_x = self.sprite_goat.x + goat_collider_radius
-		goat_center_y = self.sprite_goat.y + goat_collider_radius
-	
-		if (abs(x - goat_center_x) < goat_collider_radius) and (abs(y - goat_center_y) < goat_collider_radius):
+		return self.sprite_clicked_within_radius(x, y, self.sprite_goat, goat_collider_radius)
+
+	def wolf_clicked(self,x, y):
+		wolf_collider_radius = 50
+		return self.sprite_clicked_within_radius(x, y, self.sprite_wolf, wolf_collider_radius)
+
+	def cabbage_clicked(self,x, y):
+		cabbage_collider_radius = 40
+		return self.sprite_clicked_within_radius(x, y, self.sprite_cabbage, cabbage_collider_radius)
+
+	def farmer_clicked(self,x, y):
+		farmer_collider_radius = 40
+		return self.sprite_clicked_within_radius(x, y, self.sprite_farmer, farmer_collider_radius)
+
+	def boat_clicked(self,x, y):
+		boat_collider_radius = 80
+		return self.sprite_clicked_within_radius(x, y, self.boat, boat_collider_radius)
+
+	def goat_clicked_actions(self, x, y):
+		character_name = 'goat'
+		goat_is_in_boat = character_name in self.characters_on_board
+		if goat_is_in_boat:
+			self.set_goat_destination_to_shore()
+			self.characters_on_board.remove(character_name)
+		else:
+			self.set_goat_destination_to_boat(0, 30)
+			self.characters_on_board.append(character_name)
+		print("\nGoat was clicked")
+
+	def farmer_clicked_actions(self, x, y):
+		character_name = 'farmer'
+		farmer_is_in_boat = character_name in self.characters_on_board
+		if farmer_is_in_boat:
+			self.set_farmer_destination_to_shore()
+			self.characters_on_board.remove(character_name)
+		else:
+			self.set_farmer_destination_to_boat(70, 90)
+			self.characters_on_board.append(character_name)
+		print("\nFarmer was clicked")
+
+	def wolf_clicked_actions(self, x, y):
+		character_name = 'wolf'
+		wolf_is_in_boat = character_name in self.characters_on_board
+		if wolf_is_in_boat:
+			self.set_wolf_destination_to_shore()
+			self.characters_on_board.remove(character_name)
+		else:
+			self.set_wolf_destination_to_boat(-20, 80)
+			self.characters_on_board.append(character_name)
+		print("\nWolf was clicked")
+
+	def cabbage_clicked_actions(self, x, y):
+		character_name = 'cabbage'
+		cabbage_is_in_boat = character_name in self.characters_on_board
+		if cabbage_is_in_boat:
+			self.set_cabbage_destination_to_shore()
+			self.characters_on_board.remove(character_name)
+		else:
+			self.set_cabbage_destination_to_boat(70, 30)
+			self.characters_on_board.append(character_name)
+		print("\nCabbage was clicked")
+
+	def boat_try_ride(self, direction, x, y):
+		print('\nBoat was clicked, trying to ride!')
+		if not self.is_allowed_to_ride():
+			return
+		self.set_destinations_to_other_shore(direction)
+		if direction == -1:
+			self.global_state = 'left_shore'
+		elif direction == 1:
+			self.global_state = 'right_shore'
+
+
+	def is_allowed_to_ride(self):
+		number_of_boat_members = self.get_number_of_boat_members()
+		boat_has_driver = self.if_boat_has_driver()
+
+		print(str(number_of_boat_members) + " characters on board!")
+		print("Is driver present? " + str(boat_has_driver))
+
+		return number_of_boat_members <= 2 and boat_has_driver
+
+	def get_number_of_boat_members(self):
+		return len(self.characters_on_board)
+
+	def if_boat_has_driver(self):
+		driver_name = 'farmer'
+		return driver_name in self.characters_on_board
+
+	def sprite_clicked_within_radius(self, x, y, sprite, collider_radius):
+		center_x = sprite.x + collider_radius
+		center_y = sprite.y + collider_radius
+		if (abs(x - center_x) < collider_radius) and (abs(y - center_y) < collider_radius):
 			return True
 		return False
 
