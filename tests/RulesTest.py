ec26da8c9ef3f71417c10860507a33c62ecf845b
Coco Charles <charlesce@beloit.edu>
2020-02-21 16:18:54 -0600

    Fix minor formatting errors

--- tests/RulesTest.py
+++ tests/RulesTest.py
@@ -5,5 +5,5 @@ class RulesTest(unittest.TestCase):
     def test_read_rules(self):
         testRules = Rules('ManWolfGoatHayRules.txt')
         rules = Rules.readRules(testRules)
-        self.assertIn('items', rules)
-        # print(rules)
+        # Checks if one of the required game attributes from the file is in the dictionary
+        self.assertIn('pilot', rules)
