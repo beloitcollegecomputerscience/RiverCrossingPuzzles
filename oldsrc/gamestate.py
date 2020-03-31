8be1d9f1ad0629b2f15c3ad232a3a6a63701dd7f
Leonora Tindall <nora@nora.codes>
2020-03-30 22:38:44 -0500

    tests: remove all tests referring to shims
    That is - all tests!

--- oldsrc/gamestate.py
+++
@@ -1,8 +0,0 @@
-"""An unimplemented GameState class."""
-
-from . import Boat
-
-class GameState:
-    def __init__(self, rules, numOccupants):
-        self.boat = Boat(numOccupants)
-        self.rules = rules
