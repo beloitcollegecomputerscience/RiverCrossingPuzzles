af24a3415ef3091f5db517f105011520629748bc
Corina <sterbetc@beloit.edu>
2020-03-31 19:48:53 -0500

    Make some additional changes to GUI and Boat classes

--- riverCrossing_refactoring/GUI.py
+++ riverCrossing_refactoring/GUI.py
@@ -1,37 +1,68 @@
 import pyglet
-import time
-from pyglet.window import mouse
-import math
-import sys
-import pprint
-import animation
+
 
 class GUI(pyglet.window.Window):
+    def __init__(self, animation):
+        self.animation = animation
+        self.boat = animation.boat
+        self.scene_state = animation.scene_state
 
-	def __init__(self, animation, boat, gameState):
+        background_image = self.scene_state.get_object_by_name("background")["image"]
+        pyglet.window.Window.__init__(self, width=background_image.width,
+                                      height=background_image.height, caption="River crossing puzzle")
 
-		self.animation = animation
-		self.boat = boat
-		self.gameState = gameState
 
-		self.scene_objects = self.Animation.get_scene_objects()
-		background_image = self.animation.get_object_by_name("background")["image"]
-		pyglet.window.Window.__init__(self, width = background_image.width, height = background_image.height)
+    def on_draw(self):
+        self.scene_state.main_batch.draw()
 
-	def on_draw(self):
-		animation.main_batch.draw()
 
-	def on_mouse_press(self, x, y, button, modifiers):
-		if self.gameState.global_state == 'left_shore':
-			clicked_character = self.animation.get_clicked_character(x, y)
-			if clicked_character != None:
-				self.animation.process_clicked_character(x, y, clicked_character)
-			elif animation.boat_clicked(x, y):
-				self.boat.boat_try_ride(1, x, y)
+    def on_mouse_press(self, x, y, button, modifiers):
+        if self.scene_state.game_state != "playing":
+            return
 
-		elif gameState.global_state == 'right_shore':
-			clicked_character = self.animation.get_clicked_character(x, y)
-			if clicked_character != None:
-				self.animation.process_clicked_character(x, y, clicked_character)
-			elif self.boat_clicked(x, y):
-				self.boat.boat_try_ride(-1, x, y)
+        if self.scene_state.boat_position == 'left':
+            clicked_character = self.get_clicked_character(x, y)
+            if clicked_character is not None:
+                self.process_clicked_character(x, y, clicked_character)
+            elif self.boat_clicked(x, y):
+                self.boat.boat_try_ride(1, x, y)
+        elif self.scene_state.boat_position == 'right':
+            clicked_character = self.get_clicked_character(x, y)
+            if clicked_character is not None:
+                self.process_clicked_character(x, y, clicked_character)
+            elif self.boat_clicked(x, y):
+                self.boat.boat_try_ride(-1, x, y)
+
+
+    def get_clicked_character(self, x, y):
+        for character_name in self.animation.character_list:
+            character_object = self.scene_state.get_object_by_name(character_name)
+            collider_radius = character_object["radius"]
+            if self.scene_state.boat_position != character_object["current_shore"]:
+                continue
+            if self.sprite_clicked_within_radius(x, y, character_object["sprite"], collider_radius):
+                return character_object
+        return None
+
+
+    def process_clicked_character(self, x, y, clicked_character):
+        character_name = clicked_character["name"]
+        character_is_in_boat = character_name in self.scene_state.characters_on_board
+        if character_is_in_boat:
+            self.boat.remove_member(character_name)
+        else:
+            self.boat.add_member(character_name)
+
+
+    def boat_clicked(self, x, y):
+        boat_object = self.scene_state.get_object_by_name("boat")
+        boat_collider_radius = boat_object["radius"]
+        return self.sprite_clicked_within_radius(x, y, boat_object["sprite"], boat_collider_radius)
+
+
+    def sprite_clicked_within_radius(self, x, y, sprite, collider_radius):
+        center_x = sprite.x + collider_radius
+        center_y = sprite.y + collider_radius
+        if (abs(x - center_x) < collider_radius) and (abs(y - center_y) < collider_radius):
+            return True
+        return False
--- riverCrossing_refactoring/boat.py
+++ riverCrossing_refactoring/boat.py
@@ -1,70 +1,92 @@
-import pyglet
-import time
-import math
-import sys
-import pprint
-import animation
-
-class boat():
-
-	def __init__(self, boat_capacity, driver_name, boat_radius, animation, gameState):
-		
-		self.animation = animation
-		self.gameState = gameState
-
-		self.boat_capacity = boat_capacity
-		self.driver_name = driver_name
-		self.boat_radius = radius
-		self.boat_has_driver = False
-		self.characters_on_board = {}
+class Boat:
+    def __init__(self, boat_capacity, driver_name, boat_radius, animation, scene_state):
+        self.animation = animation
+        self.scene_state = scene_state
+        self.boat_capacity = boat_capacity
+        self.driver_name = driver_name
+        self.boat_radius = boat_radius
+        self.boat_has_driver = False
 
 
-	def add_member(character_name):
-		current_boat_members = self.get_number_of_boat_members()
-		if current_boat_members >= self.boat_capacity:
-			return
-		seat_number = self.get_available_seat_number()
-		one_seat_size = (boat_radius * 2) / self.boat_capacity
-		boat_seat_offset_y = boat_radius/2
-		boat_seat_offset_x = (seat_number * one_seat_size)
-		# centering the seat
-		boat_seat_offset_x += one_seat_size/2 - self.animation.clicked_character["radius"]
-		self.animation.set_character_destination_to_boat(clicked_character, boat_seat_offset_x, boat_seat_offset_y)
-		self.characters_on_board.update({character_name: seat_number})
+    def add_member(self, character_name):
+        character_object = self.scene_state.get_object_by_name(character_name)
+        current_boat_members = self.get_number_of_boat_members()
+        if current_boat_members >= self.boat_capacity:
+            return
+        seat_number = self.get_available_seat_number()
+        one_seat_size = (self.boat_radius * 2) / self.boat_capacity
+        boat_seat_offset_y = self.boat_radius / 2
+        boat_seat_offset_x = (seat_number * one_seat_size)
+        # centering the seat
+        boat_seat_offset_x += one_seat_size / 2 - character_object["radius"]
+        self.scene_state.object_locations.set_character_destination_to_boat(character_object,
+                                                                             boat_seat_offset_x, boat_seat_offset_y)
+        self.scene_state.characters_on_board.update({character_name: seat_number})
 
-	def remove_member(character_name):
-		self.animation.set_character_destination_to_shore(clicked_character)
-		del self.characters_on_board[character_name]
 
-	def get_available_seat_number(self):
-		occupied_seats = self.characters_on_board.values()
-		for possible_seat in range(self.boat_capacity):
-			if possible_seat not in occupied_seats:
-				return possible_seat
-		return None
+    def remove_member(self, character_name):
+        character_object = self.scene_state.get_object_by_name(character_name)
+        self.scene_state.object_locations.set_character_destination_to_shore(character_object)
+        del self.scene_state.characters_on_board[character_name]
 
-	def boat_try_ride(self, direction, x, y):
-		print('\nBoat was clicked, trying to ride!')
-		if not self.is_allowed_to_ride():
-			return		
-		if direction == -1:
-			self.gameState.global_state = 'left_shore'
-		elif direction == 1:
-			self.gameState.global_state = 'right_shore'
-		self.animation.set_destinations_to_other_shore(direction)
 
-	def is_allowed_to_ride(self):
-		number_of_boat_members = self.get_number_of_boat_members()
-		boat_has_driver = self.if_boat_has_driver()
+    def get_available_seat_number(self):
+        occupied_seats = self.scene_state.characters_on_board.values()
+        for possible_seat in range(self.boat_capacity):
+            if possible_seat not in occupied_seats:
+                return possible_seat
+        return None
 
-		print(str(number_of_boat_members) + " characters on board!")
-		print("Passengers and seats:", self.characters_on_board)
-		print("Is driver present? " + str(boat_has_driver))
 
-		return number_of_boat_members <= self.boat_capacity and boat_has_driver
+    def boat_try_ride(self, direction, x, y):
+        print('\nBoat was clicked, trying to ride!')
+        if not self.is_allowed_to_ride():
+            return
+        if direction == -1:
+            self.scene_state.boat_position = 'left'
+        elif direction == 1:
+            self.scene_state.boat_position = 'right'
+        self.scene_state.object_locations.set_destinations_to_other_shore(direction)
 
-	def get_number_of_boat_members(self):
-		return len(self.characters_on_board)
+        rule_violated = self.is_any_rule_violated(direction)
+        if rule_violated:
+            self.scene_state.game_state = "loss"
 
-	def if_boat_has_driver(self):
-		return driver_name in self.characters_on_board
+
+    def is_allowed_to_ride(self):
+        number_of_boat_members = self.get_number_of_boat_members()
+        boat_has_driver = self.if_boat_has_driver()
+
+        print(str(number_of_boat_members) + " characters on board!")
+        print("Passengers and seats:", self.scene_state.characters_on_board)
+        print("Is driver present? " + str(boat_has_driver))
+
+        return number_of_boat_members <= self.boat_capacity and boat_has_driver
+
+
+    def is_any_rule_violated(self, direction):
+        violations = self.scene_state.violations
+        characters_left = []
+
+        if direction == -1:
+            check_shore = "right"
+        else:
+            check_shore = "left"
+
+        for character_name in self.scene_state.get_character_list():
+            character_object = self.scene_state.get_object_by_name(character_name)
+            if check_shore == character_object["current_shore"]:
+                characters_left.append(character_name)
+
+        for violation in violations:
+            if set(characters_left) == set(violation):
+                return True
+        return False
+
+
+    def get_number_of_boat_members(self):
+        return len(self.scene_state.characters_on_board)
+
+
+    def if_boat_has_driver(self):
+        return self.driver_name in self.scene_state.characters_on_board
