0d88b0ee45c79bc785f10f12998d4c31bbec308a
AidanofVT <AidanofVT@gmail.com>
2020-04-09 16:29:56 -0400

    deleted stuff in preparation for attempt 4

--- tests/RulesTest.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from riverCrossingPuzzles import Rules
-
-class RulesTest(unittest.TestCase):
-    def test_read_rules(self):
-        testRules = Rules('ManWolfGoatHayRules.txt')
-        rules = Rules.readRules(testRules)
-        # Checks if one of the required game attributes from the file is in the dictionary
-        self.assertIn('pilot', rules)
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,10 +1,1 @@
-from .BoatOverfilledTest import BoatOverfilledTest
-from .BoatShoreTests import BoatShoreTests
-from .test_moveValidity import testMoveValidity
-from .BoatSpacesTests import BoatSpacesTests
-from .LocationTest import LocationTest
-from .EndGameTest import EndGameTest
-from .NewGameTest import NewGameTest
-from .SetNewGameTest import SetNewGameTest
-from .GUITest import GUITest
-from .RulesTest import RulesTest
+
