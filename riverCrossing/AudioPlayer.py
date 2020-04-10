8a71da8b78b764748cdb3b9831fa14e004899bed
truonghh99 <truonghh@beloit.edu>
2020-04-09 22:19:05 -0700

    Convert sound files to supported bit depth

--- riverCrossing/AudioPlayer.py
+++ riverCrossing/AudioPlayer.py
@@ -1,5 +1,6 @@
 import os
 import pyglet
+import soundfile
 
 
 class AudioPlayer:
@@ -8,10 +9,11 @@ class AudioPlayer:
         sounds_dir = os.path.join(working_dir, 'sounds')
         pyglet.resource.path.append(sounds_dir)
         self.sounds_dir = sounds_dir
+        self.convert_bit_depth('click.wav')
+        self.convert_bit_depth('background_music.wav')
 
         self.click_sound = pyglet.resource.media('click.wav', streaming=False)
         self.background_music = pyglet.resource.media('background_music.wav')
-
         self.media_player = pyglet.media.Player()
         self.media_player.loop = True
 
@@ -25,3 +27,12 @@ class AudioPlayer:
     def play_music_looped(self): 
         self.media_player.queue(self.background_music)        
         self.media_player.play()
+
+    def convert_bit_depth(self, file_name):
+        """
+        Pyglet only supports files with 8 or 16 bit depth. This function converts given sound
+        file to supported bit depth in case user's system preference modify the file while downloading
+        """
+        background_path = os.path.join(self.sounds_dir, file_name)
+        data, samplerate = soundfile.read(background_path)
+        soundfile.write(background_path, data, samplerate, subtype='PCM_16')
