8110a902b4b837209f29073709a638ffa91b790f
Corina Sterbet <sterbetc@beloit.edu>
2020-04-10 00:27:22 -0500

    Update AudioPlayer.py

--- riverCrossing/AudioPlayer.py
+++ riverCrossing/AudioPlayer.py
@@ -9,11 +9,13 @@ class AudioPlayer:
         sounds_dir = os.path.join(working_dir, 'sounds')
         pyglet.resource.path.append(sounds_dir)
         self.sounds_dir = sounds_dir
+        
         self.convert_bit_depth('click.wav')
         self.convert_bit_depth('background_music.wav')
-
+        
         self.click_sound = pyglet.resource.media('click.wav', streaming=False)
         self.background_music = pyglet.resource.media('background_music.wav')
+        
         self.media_player = pyglet.media.Player()
         self.media_player.loop = True
 
@@ -28,6 +30,7 @@ class AudioPlayer:
         self.media_player.queue(self.background_music)        
         self.media_player.play()
 
+        
     def convert_bit_depth(self, file_name):
         """
         Pyglet only supports files with 8 or 16 bit depth. This function converts given sound
@@ -36,3 +39,4 @@ class AudioPlayer:
         background_path = os.path.join(self.sounds_dir, file_name)
         data, samplerate = soundfile.read(background_path)
         soundfile.write(background_path, data, samplerate, subtype='PCM_16')
+        
