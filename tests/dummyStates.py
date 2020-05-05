5c9f8034323b0f324dfe89ab853f0f10a58dae4e
AidanofVT <AidanofVT@gmail.com>
2020-05-05 16:26:30 -0400

    General work. Errors have been fixed; many remain.

--- tests/dummyStates.py
+++ tests/dummyStates.py
@@ -1,4 +1,5 @@
 from riverCrossing import GameState
+from riverCrossing.Rules import Rules
 
 class DummyStates ():
 
@@ -20,12 +21,12 @@ class DummyStates ():
             DummyStates.boat2, DummyStates.right1, DummyStates.right2, DummyStates.right3, DummyStates.right4]
         print (positions)
         allPossible = []
-        if (characters.len() == 0):
+        if (len(characters) == 0):
             instanceLeft  = [positions[0], positions[1], positions[2], positions[3]]
             instanceRight  = [positions[6], positions[7], positions[8], positions[9]]
             instanceBoat = [positions[4], positions[5]]
-            print (instanceLeft + instanceBoat + instanceRight + "/n")
-            return GameState(instanceLeft, instanceBoat, instanceRight)
+            #print (''.join(instanceLeft).join(instanceBoat).join(instanceRight) + "/n")
+            return GameState(self.makeRulesList(instanceLeft, instanceBoat, instanceRight))
         else:
             for character in characters:
                 characters.remove(character)
@@ -33,6 +34,13 @@ class DummyStates ():
                     if (position == ""):
                         newPositions = positions
                         newPositions[positions.index(position)] = character
-                        allPossible.append(generate(characters, newPositions))
+                        allPossible.append(self.generate(characters, newPositions))
         print("Done generating.")
         return allPossible
+    
+    def makeRulesList (self, instanceLeft, instanceBoat, instanceRight):
+        rawWolfGoatRules = Rules("riverCrossing/ManGoatWolf.json").rules
+        rawWolfGoatRules["beginLeft"] = instanceLeft
+        rawWolfGoatRules["beginBoat"] = instanceBoat
+        rawWolfGoatRules["beginRight"] = instanceRight
+        return rawWolfGoatRules
